2e07f0b77254f30e741e59d5ddace7f83fd8ff7a
kuro112 <kuro@xtcr.net>
2015-09-15 14:45:23 -0700

    Second Commit

--- bunker.py
+++ bunker.py
@@ -12,5 +12,9 @@ def user():
     return render_template("user.html")
 
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard/index.html")
+
 if __name__ == '__main__':
     app.run(debug=True,host='127.0.0.1',threaded=True)
